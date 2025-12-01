539f4f96ce7f4d3470224daec9dda75aabf0de19
N1coG4bo <nic.lagosf@duocuc.cl>
2025-12-01 10:35:43 -0300

    Se agrega inicio, fotos, y se da paso a crear estructura, e importacioens de css

---
+++ src/pages/inicio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import NavBarPrincipal from './components/navbar';
+
+function Inicio() {
+    return(
+        <div>
+            <NavBarPrincipal/>
+        </div>
+    );
+
+}
+export default Inicio;
