b06df578414153e4bbcf1c4e01afbb61f2d7df93
stredes <gianlucassanmartin@gmail.com>
2026-01-03 17:39:59 -0300

    03012026

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Punto de arranque de React, monta el árbol de la app.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
